6873a17959a36de432347af4f1eac06b4332f215
robertIsaac <dev.robert.isaac@gmail.com>
2024-07-20 22:38:35 +0300

    simplify the code

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,5 +1,4 @@
-import { Component, effect, input, signal } from '@angular/core';
-import { CommentService } from '../comment.service';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-comment',
@@ -9,22 +8,5 @@ import { CommentService } from '../comment.service';
   styleUrl: './comment.component.css'
 })
 export class CommentComponent {
-  readonly id = input.required<number>();
-
-  protected readonly comments = signal<{ id: number }[]>([]);
-
-  constructor(private readonly commentService: CommentService) {
-    effect(
-      () => {
-        this.getComments();
-      },
-      { allowSignalWrites: true },
-    );
-  }
-
-  private getComments() {
-    this.commentService.getComments(this.id()).subscribe((questionComments) => {
-      this.comments.set(questionComments);
-    });
-  }
+  protected readonly comments = signal<{ id: number }[]>([{id: 1}]);
 }
