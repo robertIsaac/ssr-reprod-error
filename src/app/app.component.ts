d7c529f49ffcfa8b6815539016679d5aba1dc2d4
robertIsaac <dev.robert.isaac@gmail.com>
2024-07-20 22:42:19 +0300

    simplify the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { QuestionComponent } from './question/question.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [QuestionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ssr-reprod-error';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import {
-  provideRouter,
-  withComponentInputBinding,
-  withEnabledBlockingInitialNavigation,
-  withViewTransitions,
-} from '@angular/router';
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withEnabledBlockingInitialNavigation(),
-      withComponentInputBinding(),
-      withViewTransitions(),
-    ),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
   ]
 };
