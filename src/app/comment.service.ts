eeb7df7775eb2818cc3ed3d635908e8af0b1030b
robertIsaac <dev.robert.isaac@gmail.com>
2024-07-20 22:35:17 +0300

    simplify the code

--- src/app/comment.service.ts
+++ src/app/comment.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
-import { environment } from './environment/environment';
-import { HttpClient } from '@angular/common/http';
-import { delay, Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor(private readonly httpClient: HttpClient) {}
-
   getComments(id: number): Observable<{ id: number }[]> {
-    const url = `${environment.apiUrl}comment/${id}.json`;
-    return this.httpClient.get<{ id: number }[]>(url).pipe(delay(500));
+    return of([{
+        "id": 1
+      }]
+    )
   }
 }
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,5 +1,4 @@
-import { Component, effect, input, numberAttribute, signal } from '@angular/core';
-import { QuestionService } from '../question.service';
+import { Component, signal } from '@angular/core';
 import { CommentComponent } from '../comment/comment.component';
 
 @Component({
@@ -12,29 +11,7 @@ import { CommentComponent } from '../comment/comment.component';
   styleUrl: './question.component.css'
 })
 export class QuestionComponent {
-  protected readonly questionId = input.required({
-    transform: numberAttribute,
-  });
-
   protected readonly question = signal<{id: number} | undefined>(
-    undefined,
+    {id: 1},
   );
-
-  constructor(private readonly questionService: QuestionService) {
-    effect(
-      () => {
-        this.refreshQuestion();
-      },
-      { allowSignalWrites: true },
-    );
-  }
-
-  protected refreshQuestion(): void {
-    this.questionService
-      .getQuestion(this.questionId())
-      .subscribe((question) => {
-        this.question.set(question);
-      });
-  }
-
 }
